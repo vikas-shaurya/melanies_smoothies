b3cd446deba2ff004ea906705a1e9f827b49b5d4
vikas-shaurya <shauryashopping@gmail.com>
2026-05-23 12:52:34 -0400

    Update streamlit_app.py
    Moved smoothiefroot_response and dataframe section under fruit_chosen for loop section.

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,6 +27,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+        sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=True)
+
 
     #st.write(ingredients_string)
 
@@ -43,6 +46,3 @@ if time_to_insert:
     st.success('Your Smoothie is ordered,'+name_on_order+'!',icon="✅")
 
 import requests  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=True)
